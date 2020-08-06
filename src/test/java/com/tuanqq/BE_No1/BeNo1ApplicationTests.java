6c7db312e07df503d127b35a24ba3f8ac2e256c6
TINHVAN\tuanqq <tuanboomcn4ptit@gmail.com>
2020-08-06 11:06:19 +0700

    first commit

---
+++ src/test/java/com/tuanqq/BE_No1/BeNo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanqq.BE_No1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeNo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
